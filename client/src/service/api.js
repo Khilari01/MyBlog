ce3d132f432a76e5c9b64b7ea5a6e6af91ea8390
Khilari01 <khilariatharva01@gmail.com>
2024-01-03 16:08:04 +0530

    fourth update

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES , SERVICE_URLS } from '../constants/config.js';   
 import { getAccessToken , getType } from '../utils/common-utils.js';
 
-const API_URL = 'http://localhost:8005';
+const API_URL = 'https://my-blog-server-five.vercel.app/';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
